213530f276c40dd0b3f7263538c4caaede0ea51d
ADM <ADM@MICRO143>
2017-01-13 21:20:01 -0200

    Projeto novo.

---
+++ projeto/Treinar/src/TerceiraClasse.java
@@ -0,0 +1,30 @@
+
+public class TerceiraClasse {
+
+	public static void main(String[] args) {
+		byte a = 127;
+		System.out.println(a);
+		short b = 12589;
+		System.out.println(b);
+		int c = 123456789;
+		System.out.println(c);
+		long d = 123456789123456789L;
+		System.out.println(d);
+		float e = 1.256f;
+		System.out.println(e);
+		double f = 1.589;
+		System.out.println(f);
+		float f1 = 234.5f;
+		System.out.println(f1);
+		boolean quente = false;
+		boolean frio = true;
+		char w = 'B';
+		char letraV = 66;
+		System.out.println(w);
+		System.out.println(letraV);
+		System.out.println("Ola Mundo");
+		
+	
+				
+	}
+}
